c0768f7f0aa88dfbc2a47a3460ed013cb499f4af
AnmolAqsa <165940283+AnmolAqsa@users.noreply.github.com>
2024-07-04 12:42:39 -0700

    Add files via upload
    Q19, 45 Projects

---
+++ main.js
@@ -0,0 +1,6 @@
+/*
+Q19. Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number
+of people you are inviting to dinner.
+*/
+var Guests = ["ali", "sara"];
+console.log("Total number of guest: ".concat(Guests.length, " \n"));
---
+++ main.ts
@@ -0,0 +1,6 @@
+/*
+Q19. Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number
+of people you are inviting to dinner.
+*/
+let Guests = ["ali", "sara"];
+console.log(`Total number of guest: ${Guests.length} \n`);
